2f4cb6a34cdc8f88a22bb6a4f4c2fea8dc0a5607
Adam Fairhead <adam@fairheadcreative.com>
2012-09-06 15:58:32 -0400

    basic f3-v65 integration

--- assets/javascripts/foundation/app.js
+++ assets/javascripts/foundation/app.js
@@ -1,6 +1,15 @@
 (function ($) {  
 
   $(function(){
+
+    $('.v65-selected').parent().addClass('active');
+    $('.primary-links > ul').addClass('nav-bar');
+    $('.primary-links > ul ul').addClass('flyout');
+    $('.primary-links > ul ul').parent().addClass('has-flyout text-left');
+    $('.primary-links > ul > ul').parent().find('a').after('<a href="#" class="flyout-toggle"><span></span></a>');
+    $('.footer-links > ul > li:last-child').after('<li><a href="#">Back to Top</a></li>');
+    $('article > p:first').addClass('lead');
+    
     $(document).foundationAlerts();
     $(document).foundationButtons();
     $(document).foundationAccordion();
@@ -12,11 +21,14 @@
     $(document).tooltips();
     $('input, textarea').placeholder();
     
-    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+    $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+    $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+    $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+    $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+  
+    
+    
+    
   });
   
 })(jQuery);
